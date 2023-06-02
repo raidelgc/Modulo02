1288209e98f670740b70bbefaca1a85ed9cd8cb2
raidelgc <raidelgc93@gmail.com>
2023-06-01 21:52:17 -0300

    Personalizei a página inicial com 'Hello World'

---
+++ Semana05/meu-projeto/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  document.title = 'Meu Projeto';
+  return (
+    <div className="App">
+      
+      
+        <header>Hello World</header>
+      
+    </div>
+  );
+}
+
+export default App;
